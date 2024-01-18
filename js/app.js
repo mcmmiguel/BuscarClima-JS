8159536247d2f3446a7dbef07f1199e88a6a62bb
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-17 19:55:21 -0600

    Consulta a la API

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ function buscarClima(e) {
 
 
     // Consultar API
+    consultarAPI(ciudad, pais);
 };
 
 function mostrarError(mensaje) {
@@ -33,7 +34,7 @@ function mostrarError(mensaje) {
         alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
 
         alerta.innerHTML = `
-            <strong class="font-bold">Error</strong>
+            <strong class="font-bold line-text">Error</strong>
             <span class"block">${mensaje}</span>
             `;
 
@@ -44,5 +45,19 @@ function mostrarError(mensaje) {
             alerta.remove();
         }, 5000);
     }
+};
+
+async function consultarAPI(ciudad, pais) {
+    const apiKey = 'd08590422e6253bd0e931cc2b9133511';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiKey}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            if (datos.cod === '404') {
+                mostrarError('No se pudo encontrar información de esta ciudad')
+            }
+        })
 
 }
