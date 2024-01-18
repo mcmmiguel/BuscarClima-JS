6b342821987cc272efbfa6bb506847bdd3a29d36
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-17 20:10:10 -0600

    Mostrar temperatura

--- js/app.js
+++ js/app.js
@@ -55,9 +55,37 @@ async function consultarAPI(ciudad, pais) {
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
+            limpiarHTML(); //Limpiar el HTML previo
             if (datos.cod === '404') {
-                mostrarError('No se pudo encontrar información de esta ciudad')
+                mostrarError('No se pudo encontrar información de esta ciudad');
+                return;
             }
-        })
 
-}
+            // imprime la respuesta en HTML
+            mostrarClima(datos);
+
+        })
+};
+
+function mostrarClima(datos) {
+    const { main: { temp, temp_max, temp_min } } = datos;
+    const centigrados = kelvinACentigrados(temp);
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(actual);
+
+    resultado.appendChild(resultadoDiv);
+};
+
+const kelvinACentigrados = (grados) => parseInt(grados - 273.15);
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+};
